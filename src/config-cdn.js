6104f94512eac10687044e756152599939668a6a
ggmtecit-prog <ggmtecit@gmail.com>
2026-01-29 09:59:52 +0000

    Updates

--- src/config-cdn.js
+++ src/config-cdn.js
@@ -115,6 +115,8 @@ tailwind.config = {
             },
             borderRadius: {
                 'none': '0',
+                '3xs': '2px',
+                '2xs': '4px',
                 'xs': '6px',
                 'sm': '8px',
                 'md': '12px',
--- src/tokens/radius.js
+++ src/tokens/radius.js
@@ -1,5 +1,7 @@
 export const radius = {
     'none': '0',
+    '3xs': '2px',
+    '2xs': '4px',
     'xs': '6px',
     'sm': '8px',
     'md': '12px',
